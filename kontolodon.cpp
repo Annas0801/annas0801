4349472dcdf6fd6450c0df43fb998968ee027ba8
Annas0801 <annas0801@students.amikom.ac.id>
2022-03-17 17:06:35 +0700

    JANGAN REWEL YA BEB

---
+++ kontolodon.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    int nilaiArray[4] = {21,25,77,80};
+    for(int i=0; i<4; i++){
+        cout<<"NILAI ARRAY komtold KE #"<< i << "=" << nilaiArray[i]<<endl;
+    }
+}
